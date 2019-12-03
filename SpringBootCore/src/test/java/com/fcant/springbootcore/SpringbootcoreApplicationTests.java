f940112069732729e282cab68d53f1cba626e6bd
Fcant <fcscanf@outlook.com>
2019-12-03 09:45:17 +0800

    Initial commit

---
+++ SpringBootCore/src/test/java/com/fcant/springbootcore/SpringbootcoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fcant.springbootcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootcoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
