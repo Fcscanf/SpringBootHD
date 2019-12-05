e07eaee33f30ddf85268d4f5df59790004abe47e
Fcant <fcscanf@outlook.com>
2019-12-05 09:21:25 +0800

    [Code-Updates-SpringBootExpand]:SpringBootExpand项目构建-该Pro主要与配置文件相关，获取配置文件的值，将Java类注入到配置中

---
+++ SpringBootExpand/src/main/java/com/fcant/springbootexpand/SpringBootExpandApplication.java
@@ -0,0 +1,13 @@
+package com.fcant.springbootexpand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootExpandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootExpandApplication.class, args);
+	}
+
+}
---
+++ SpringBootExpand/src/test/java/com/fcant/springbootexpand/SpringbootexpandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fcant.springbootexpand;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootexpandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
