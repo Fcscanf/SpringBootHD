bd781a63ff07e59ec36314fb8a82cf1d31bac035
Fcant <fcscanf@outlook.com>
2019-12-08 22:40:40 +0800

    [Codes-Updates-SpringCloud Eureka Server]:Init Pro

---
+++ SpringCloudEurekaSever/src/main/java/com/fcant/springcloudeurekasever/SpringCloudEurekaSeverApplication.java
@@ -0,0 +1,13 @@
+package com.fcant.springcloudeurekasever;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudeurekaseverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudeurekaseverApplication.class, args);
+	}
+
+}
---
+++ SpringCloudEurekaSever/src/test/java/com/fcant/springcloudeurekasever/SpringcloudeurekaseverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fcant.springcloudeurekasever;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudeurekaseverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
